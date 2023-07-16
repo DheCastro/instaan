045afedf8daacd2a6c870d2496459bcfbf4de8d5
Dhellano de Castro Oliveira <dhecastro90@gmail.com>
2023-07-16 17:38:27 -0300

    initial commit

---
+++ src/main/java/br/com/dhecastro/instaan/InstaanApplication.java
@@ -0,0 +1,13 @@
+package br.com.dhecastro.instaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dhecastro/instaan/InstaanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dhecastro.instaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
